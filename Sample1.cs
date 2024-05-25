0e4277d20e53605e36b3449a7af1a9b43521b157
강지우 <82877633+jiwooriver@users.noreply.github.com>
2024-05-25 17:23:50 +0900

    2.1 콘솔로 출력

---
+++ Sample1.cs
@@ -0,0 +1,13 @@
+// 콘솔 화면에 문자열을 출력한다
+using System;
+
+class Sample1
+{
+    public static void Main()
+    {
+        // 화면에 문자를 출력한다
+        Console.WriteLine("어서 오세요 C#으로!");
+        Console.WriteLine("C#을 시작합시다!");
+        
+    }
+}
