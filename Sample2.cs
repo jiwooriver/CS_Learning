23c258bb70785b6e502266dda99354573ef31e46
jiwooriver <jiwooriver@gmail.com>
2024-05-25 17:48:36 +0900

    2.2 폼

---
+++ Sample2.cs
@@ -0,0 +1,15 @@
+// 윈도에 출력한다
+using System.Windows.Forms;
+
+class Sample2
+{
+    public static void Main()
+    {
+        Form fm;
+        fm = new Form();
+
+        fm.Text();
+
+        Application.Run(fm);
+    }
+}
